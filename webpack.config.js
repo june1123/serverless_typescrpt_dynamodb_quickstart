c98bfcb8b216ae026a5445369f29dd89f8a06f09
Jacob <june.choi.1123@gmail.com>
2017-06-08 11:51:16 +0900

    Add comments for multiple entires in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ var nodeExternals = require('webpack-node-externals');
 module.exports = {
   context: path.join(__dirname, "src"),
   entry: [
-    'dynogels', // To include external modules.
+    // To include external module,
+    // Dependency is not packaged when it was not require or import in code when webpackIncludeModules is enabled.
+    // Remain a 'dynogels' entry until merge pull request. (https://github.com/elastic-coders/serverless-webpack/pull/90)
+    'dynogels', // To include external modules. wait merge
     "./Handler.ts"
   ],
   target: 'node',
